a63cb6bdbce1ba73573051709662caa86c3bbe0c
YuTera58 <YuTerakoya4Study@gmail.com>
2025-01-12 15:57:55 +0900

    マウスイベント追加

--- js/event.js
+++ js/event.js
@@ -1,6 +1,36 @@
+// JQuery でイベント処理
 $(function() {
   // ボタンがクリックされた場合
   $('button').on('click', function(){
     console.log('ボタンがクリックされました');
   });
  });
+
+$(function() {
+  $('div').on({
+    'click': () => {
+      // 背景を赤 
+      $('div').css('background-color', 'red');
+      // テキストを「click」 
+      $('div').text('click');
+    },
+    'dblclick': () => { 
+      // 背景を緑 
+      $('div').css('background-color', 'green'); 
+      // テキストを「dblclick」 
+      $('div').text('dblclick');
+    },
+    'mouseenter': () => {
+      // 背景を青 
+      $('div').css('background-color', 'blue'); 
+      // テキストを「mouseenter」 
+      $('div').text('mouseenter');
+      },
+      'mouseout': () => {
+      // 背景をグレー 
+      $('div').css('background-color', 'gray'); 
+      // テキストを「mouseout」 
+      $('div').text('mouseout');
+    },
+  });
+});
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// JQuery で属性を指定して処理を行う
 $(function() {
   // class属性値の先頭が「sample」の要素だけを抽出する
   const text = $('li[class^="sample"]').text();
